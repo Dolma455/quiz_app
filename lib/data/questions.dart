cc506095d3017e82a2bf561deba0cba411a100f4
Dolma <dolma1918@gmail.com>
2023-12-06 12:25:12 +0545

    feat: Accessing questions and answers from the list

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -7,35 +7,35 @@ final questions = [
     'Blocks',
     'Components',
   ]),
-   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
-    'Widget',
-    'Function',
-    'Blocks',
-    'Components',
+  QuizQuestion(
+      text: 'What are the msot useful editors for flutter apps?',
+      answers: [
+        'Visual Studio',
+        'Android Studio',
+        'Pycharm',
+        'Visual Studio Code',
+      ]),
+  QuizQuestion(text: 'What are the resources to learn flutter?', answers: [
+    'Notes',
+    'Documentation',
+    'Books',
+    'Articles',
   ]),
-   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
-    'Widget',
-    'Function',
-    'Blocks',
-    'Components',
+  QuizQuestion(text: 'In what technology is flutter built?', answers: [
+    'C++',
+    'C',
+    'Dart',
+    'Skia-2D',
   ]),
-   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
-    'Widget',
-    'Function',
-    'Blocks',
-    'Components',
+  QuizQuestion(text: 'What build modes are availbale in flutter?', answers: [
+    'Mode',
+    'Debug Mode',
+    'Profile Mode',
+    'Release Mode',
   ]),
-   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
-    'Widget',
-    'Function',
-    'Blocks',
-    'Components',
+  QuizQuestion(text: 'Explain the flutter architecture', answers: [
+    'Upper Layer',
+    'Middle Layer',
+    'Bottom Layer',
   ]),
-   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
-    'Widget',
-    'Function',
-    'Blocks',
-    'Components',
-  ]),
-
 ];
--- lib/screen/questions_screen.dart
+++ lib/screen/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_ad_basics/data/questions.dart';
 import 'package:quiz_app_ad_basics/widget/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -11,33 +12,34 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The questions',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
             ),
           ),
           const SizedBox(
             height: 30,
           ),
-          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
           const SizedBox(
             height: 16,
           ),
-          AnswerButton(answerText: 'Answer 2', onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
           const SizedBox(
             height: 16,
           ),
-          AnswerButton(answerText: 'Answer 3', onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
           const SizedBox(
             height: 16,
           ),
-          AnswerButton(answerText: 'Answer 4', onTap: () {})
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {})
         ],
       ),
     );
