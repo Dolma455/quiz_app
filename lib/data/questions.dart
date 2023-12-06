7c79e99c321cc5293613399f2db87ade7ef28290
Dolma <dolma1918@gmail.com>
2023-12-06 12:01:50 +0545

    feat: Answer button created with styling

---
+++ lib/data/questions.dart
@@ -0,0 +1,41 @@
+import 'package:quiz_app_ad_basics/models/quiz_questions.dart';
+
+final questions = [
+  QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
+    'Widget',
+    'Function',
+    'Blocks',
+    'Components',
+  ]),
+   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
+    'Widget',
+    'Function',
+    'Blocks',
+    'Components',
+  ]),
+   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
+    'Widget',
+    'Function',
+    'Blocks',
+    'Components',
+  ]),
+   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
+    'Widget',
+    'Function',
+    'Blocks',
+    'Components',
+  ]),
+   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
+    'Widget',
+    'Function',
+    'Blocks',
+    'Components',
+  ]),
+   QuizQuestion(text: 'What are the building blocks of flutter UIs?', answers: [
+    'Widget',
+    'Function',
+    'Blocks',
+    'Components',
+  ]),
+
+];
---
+++ lib/models/quiz_questions.dart
@@ -0,0 +1,11 @@
+class QuizQuestion {
+  final String text;
+  final List<String> answers;
+
+  QuizQuestion({
+    required this.text,
+    required this.answers,
+  });
+
+  
+}
